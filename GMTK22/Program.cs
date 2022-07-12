18ae43ed624b8164f6a1d3d151988793e47d9949
notexplosive <thenotexplosive@gmail.com>
2022-07-11 21:00:03 -0700

    (Machina:Automated) Initial Commit

---
+++ GMTK22/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GMTK22
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
