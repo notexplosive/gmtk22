17e0b92fdd1eed8a1aabe0cd85cef93838cfd810
notexplosive <thenotexplosive@gmail.com>
2022-07-16 18:25:38 -0700

    Formatter

--- GMTK22/DieCartridge.cs
+++ GMTK22/DieCartridge.cs
@@ -12,36 +12,37 @@ namespace GMTK22
 {
     public class DieCartridge : GameCartridge
     {
-        public static GameCore GameCore { get; private set; }
-
         public DieCartridge() : base(new Point(1600, 900), ResizeBehavior.KeepAspectRatio)
         {
         }
+
+        public static GameCore GameCore { get; private set; }
 
         public override void OnGameLoad(GameSpecification specification, MachinaRuntime runtime)
         {
             SceneLayers.BackgroundColor = Color.ForestGreen;
 
             // Setup
-            GameCore = new GameCore(SceneLayers, Random.Seed);
-            
+            DieCartridge.GameCore = new GameCore(SceneLayers, Random.Seed);
+
             // UI
             var layout = LayoutNode.VerticalParent("", LayoutSize.Pixels(1600, 900), LayoutStyle.Empty,
                 LayoutNode.Leaf("MoneyCounter", LayoutSize.StretchedHorizontally(120)),
                 LayoutNode.Leaf("Tooltip", LayoutSize.StretchedBoth()),
-                LayoutNode.OneOffParent("BuildMenuWrapper", LayoutSize.StretchedHorizontally(200), new LayoutStyle(margin: new Point(5)),
+                LayoutNode.OneOffParent("BuildMenuWrapper", LayoutSize.StretchedHorizontally(200),
+                    new LayoutStyle(new Point(5)),
                     LayoutNode.Leaf("BuildMenu", LayoutSize.StretchedBoth())
                 )
             ).Bake();
 
-            var uiLayoutActors = new LayoutActors(GameCore.UiScene, layout);
-            
+            var uiLayoutActors = new LayoutActors(DieCartridge.GameCore.UiScene, layout);
+
             // Money counter
             var moneyCounter = uiLayoutActors.GetActor("MoneyCounter");
             new BoundedTextRenderer(moneyCounter, "0", MachinaClient.Assets.GetSpriteFont("UIFont"), Color.White,
                 Alignment.Center);
-            new MoneyTracker(moneyCounter, GameCore.Player);
-            
+            new MoneyTracker(moneyCounter, DieCartridge.GameCore.Player);
+
             // Build menu
             var buildMenuActor = uiLayoutActors.GetActor("BuildMenu");
             new Hoverable(buildMenuActor);
@@ -49,27 +50,27 @@ namespace GMTK22
 
             // Selector
             var selector = new BuildingSelector(buildMenu);
-            
+
             // Tooltip
             var tooltipActor = uiLayoutActors.GetActor("Tooltip");
             new Tooltip(tooltipActor, buildMenu, selector);
-            
+
             // WORLD
             var buildingMap = new BuildingMap(selector);
 
             buildMenu.RequestedBuilding += buildingMap.BuildFromCommand;
-            
+
             // Starting Die
-            BuildingPosition position = new BuildingPosition(Point.Zero);
-            
-            buildingMap.CreateBuildSite(new BuildingPosition(position.GridPosition + new Point(0,0)));
-            buildingMap.CreateBuildSite(new BuildingPosition(position.GridPosition + new Point(-1,0)));
-            buildingMap.CreateBuildSite(new BuildingPosition(position.GridPosition + new Point(-2,0)));
-            buildingMap.CreateBuildSite(new BuildingPosition(position.GridPosition + new Point(1,0)));
-            buildingMap.CreateBuildSite(new BuildingPosition(position.GridPosition + new Point(2,0)));
-            
-            #if DEBUG
-            new Cheats(GameCore.UiScene.AddActor("Cheat"));
+            var position = new BuildingPosition(Point.Zero);
+
+            buildingMap.CreateBuildSite(new BuildingPosition(position.GridPosition + new Point(0, 0)));
+            buildingMap.CreateBuildSite(new BuildingPosition(position.GridPosition + new Point(-1, 0)));
+            buildingMap.CreateBuildSite(new BuildingPosition(position.GridPosition + new Point(-2, 0)));
+            buildingMap.CreateBuildSite(new BuildingPosition(position.GridPosition + new Point(1, 0)));
+            buildingMap.CreateBuildSite(new BuildingPosition(position.GridPosition + new Point(2, 0)));
+
+#if DEBUG
+            new Cheats(DieCartridge.GameCore.UiScene.AddActor("Cheat"));
 #endif
         }
 
